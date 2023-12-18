3131d3262abeabca67179fb6ea07697fab6367d6
Heilly <heilly.perdomo.910812@gmail.com>
2023-12-19 00:17:40 +0100

    folder structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Gifs App',
+        loadComponent: () => import('./gifs/pages/layout/layout.component'),
+    },
+    {
+        path: '**',
+        redirectTo: '',
+        pathMatch: 'full'
+    }
+];
---
+++ src/app/gifs/pages/layout/layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  standalone: true,
+  imports: [CommonModule],
+})
+export default class LayoutComponent {}
